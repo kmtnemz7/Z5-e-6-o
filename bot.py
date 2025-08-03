d2a0feeb1d738c58f2f33364a1a7840d43fe158c
kmtnemz7 <apattas789@gmail.com>
2025-08-04 07:51:04 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,14 @@ import os
 from telethon import TelegramClient, events
 
 api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
+api_hash = os.getenv("API_HASH"))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 SOURCE_GROUP = os.getenv("BACKEND_GROUP", "zeropingphan")
 TARGET_GROUP = os.getenv("FRONTEND_GROUP", "ZeroPingX")
 
-# ✅ Correct way to init bot client
+# Init bot client
 bot = TelegramClient("zeroping_bot", api_id, api_hash).start(bot_token=BOT_TOKEN)
-
-from telethon import events
-
-# Main handler
 
 @bot.on(events.NewMessage(chats=SOURCE_GROUP))
 async def handle(event):
@@ -21,10 +17,10 @@ async def handle(event):
     if not msg or not msg.text:
         return
 
-    await client.send_message(
+    await bot.send_message(
         TARGET_GROUP,
         msg.text,
         parse_mode="md"  # or "MarkdownV2" if needed
     )
-bot.run_until_disconnected()
 
+bot.run_until_disconnected()
